41889997ddc5793a62af70ea463d846684adabed
Jack Franklin <jack@jackfranklin.co.uk>
2011-09-03 21:25:04 +0100

    API Call Working

--- a.coffee
+++ a.coffee
@@ -1,12 +1,30 @@
 class WordMemory 
     constructor: (startButton)->
         @startButton = startButton
+        @currentLevel = 0
+        @currentScore = 0
+        @currentWords = []
+        @wordsGot = []
+        @wordnik = "03e4c5fc37b23b29ff30f0af73c00c5302cff40d81eb8b139"
         @init()
 
     init: ->
-        $(@startButton).click (e) ->
+        $(@startButton).click (e) =>
             e.preventDefault()
-            console.log "hi"
+            @collectWords(5)
+
+    collectWords: (amount=5) ->
+        $.ajax({
+            url: "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&limit=" + amount + "&maxLength=8&minLength=2&api_key=" + @wordnik,
+            dataType: "json",
+            method: "get",
+            async: "false",
+            success: (d) ->
+                console.log d
+                i = 0
+                @currentWords.push d[i].word while i++ < d.length
+                
+        }); 
 
 
 
--- a.js
+++ a.js
@@ -1,14 +1,41 @@
 (function() {
   var WordMemory, game;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   WordMemory = (function() {
     function WordMemory(startButton) {
       this.startButton = startButton;
+      this.currentLevel = 0;
+      this.currentScore = 0;
+      this.currentWords = [];
+      this.wordsGot = [];
+      this.wordnik = "03e4c5fc37b23b29ff30f0af73c00c5302cff40d81eb8b139";
       this.init();
     }
     WordMemory.prototype.init = function() {
-      return $(this.startButton).click(function(e) {
+      return $(this.startButton).click(__bind(function(e) {
         e.preventDefault();
-        return console.log("hi");
+        return this.collectWords(5);
+      }, this));
+    };
+    WordMemory.prototype.collectWords = function(amount) {
+      if (amount == null) {
+        amount = 5;
+      }
+      return $.ajax({
+        url: "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&limit=" + amount + "&maxLength=8&minLength=2&api_key=" + this.wordnik,
+        dataType: "json",
+        method: "get",
+        async: "false",
+        success: function(d) {
+          var i, _results;
+          console.log(d);
+          i = 0;
+          _results = [];
+          while (i++ < d.length) {
+            _results.push(this.currentWords.push(d[i].word));
+          }
+          return _results;
+        }
       });
     };
     return WordMemory;
